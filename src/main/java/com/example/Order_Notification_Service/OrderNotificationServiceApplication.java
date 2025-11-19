95458c5d20953c4435edaf631e690648429ccfcb
Pranav Thakur <thakur.pranav109@gmail.com>
2025-11-20 02:42:09 +0530

    initial commit

---
+++ src/main/java/com/example/Order_Notification_Service/OrderNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Order_Notification_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Order_Notification_Service/OrderNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Order_Notification_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
